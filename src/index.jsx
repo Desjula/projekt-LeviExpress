ac92d3a0bc9cf4a18ac44404acf5fcddff7b6b50
Julie Desová <juliedesova@seznam.cz>
2023-04-06 18:59:00 +0200

    routing sorted out

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './components/App';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { App } from "./components/App";
+import { Home } from './components/Home';
+import { Reservation } from "./components/Reservation";
 import './style.css';
 
 export const API_BASE_URL = 'https://apps.kodim.cz/daweb/leviexpress/api';
 
-createRoot(document.querySelector('#app')).render(<App />, );
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/reservation",
+        element: <Reservation />,
+      },
+    ]
+  },
+
+]);
+
+createRoot(document.querySelector('#app')).render(
+  <RouterProvider router={routes} />
+);

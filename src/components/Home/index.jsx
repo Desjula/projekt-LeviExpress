5c37db4607a8c1004214655f7a461d541ed9347b
Julie Desová <juliedesova@seznam.cz>
2023-04-13 19:27:49 +0200

    vyber startovni a cilove stanice

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { JourneyPicker } from '../JourneyPicker';
 
-export const Home = () => (
-  <main>
-    <JourneyPicker />
-  </main>
-);
+export const Home = () => {
+
+  const [journeyResult, setJourneyResult] = useState(null)
+
+  const handleJourneyChange = ( {results} ) => {
+    setJourneyResult(results)
+    console.log(results)
+  };
+
+
+  return(
+    <main>
+      <JourneyPicker onJourneyChange={handleJourneyChange} />
+      {journeyResult && `Nalezeno spojení s id ${journeyResult.journeyId}.`}
+    </main>
+)};
--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -1,79 +1,84 @@
-import React, { useEffect, useState } from 'react';
-import mapImage from './img/map.svg';
-import './style.css';
+import React, { useEffect, useState } from "react";
+import mapImage from "./img/map.svg";
+import { CityOptions } from "./CityOptions";
+import "./style.css";
+import { DatesOptions } from "./DatesOptions";
 
 export const JourneyPicker = ({ onJourneyChange }) => {
+  const [fromCity, setFromCity] = useState("");
+  const [toCity, setToCity] = useState("");
+  const [date, setDate] = useState("");
+  const [cities, setCities] = useState([]);
+  const [dates, setDates] = useState([]);
+
+  useEffect(() => {
+    fetch("https://apps.kodim.cz/daweb/leviexpress/api/cities")
+      .then((response) => response.json())
+      .then((data) => setCities(data.results));
+
+    fetch("https://apps.kodim.cz/daweb/leviexpress/api/dates")
+      .then((response) => response.json())
+      .then((data) => setDates(data.results));
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Odesílám fromulář s cestou")
-    console.log('Form submitted:', fromCity, toCity, date);
-  }
+    fetch(
+      `https://apps.kodim.cz/daweb/leviexpress/api/journey?fromCity=${fromCity}&toCity=${toCity}&date=${date}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        onJourneyChange(data);
+      });
+  };
 
-const [fromCity, setFromCity] = useState("");
-const [toCity, setToCity] = useState("");
-const [date, setDate] = useState("");
+  const handleFromCityChange = (e) => {
+    setFromCity(e.target.value);
+  };
 
-const handleFromCityChange = (e) => {
-  setFromCity(e.target.value);
-};
+  const handleToCityChange = (e) => {
+    setToCity(e.target.value);
+  };
 
-const handleToCityChange = (e) => {
-  setToCity(e.target.value);
-};
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
 
-const handleDateChange = (e) => {
-  setDate(e.target.value);
-};
-
-return(
-  <div className="journey-picker container">
-    <h2 className="journey-picker__head">Kam chcete jet?</h2>
-    <div className="journey-picker__body">
-      <form className="journey-picker__form" onSubmit={handleSubmit}>
-        <label>
-          <div className="journey-picker__label">Odkud:</div>
-          <select value={fromCity} onChange={handleFromCityChange}>
-            <option value="">Vyberte</option>
-            <option value="mesto01">Město 01</option>
-            <option value="mesto02">Město 02</option>
-            <option value="mesto03">Město 03</option>
-            <option value="mesto04">Město 04</option>
-            <option value="mesto05">Město 05</option>
-          </select>
-        </label>
-        <label>
-          <div className="journey-picker__label">Kam:</div>
-          <select value={toCity} onChange={handleToCityChange}>
-            <option value="">Vyberte</option>
-            <option value="mesto01">Město 01</option>
-            <option value="mesto02">Město 02</option>
-            <option value="mesto03">Město 03</option>
-            <option value="mesto04">Město 04</option>
-            <option value="mesto05">Město 05</option>
-          </select>
-        </label>
-        <label>
-          <div className="journey-picker__label">Datum:</div>
-          <select value={date} onChange={handleDateChange}>
-            <option value="">Vyberte</option>
-            <option value="datum01">Datum 01</option>
-            <option value="datum02">Datum 02</option>
-            <option value="datum03">Datum 03</option>
-            <option value="datum04">Datum 04</option>
-            <option value="datum05">Datum 05</option>
-          </select>
-        </label>
-        <div className="journey-picker__controls">
-          <button 
-            className="btn" 
-            type="submit"
-          > 
-            Vyhledat spoj
-          </button>
-        </div>
-      </form>
-      <img className="journey-picker__map" src={mapImage} />
+  return (
+    <div className="journey-picker container">
+      <h2 className="journey-picker__head">Kam chcete jet?</h2>
+      <div className="journey-picker__body">
+        <form className="journey-picker__form" onSubmit={handleSubmit}>
+          <label>
+            <div className="journey-picker__label">Odkud:</div>
+            <select value={fromCity} onChange={handleFromCityChange}>
+              <CityOptions cities={cities} />
+            </select>
+          </label>
+          <label>
+            <div className="journey-picker__label">Kam:</div>
+            <select value={toCity} onChange={handleToCityChange}>
+              <CityOptions cities={cities} />
+            </select>
+          </label>
+          <label>
+            <div className="journey-picker__label">Datum:</div>
+            <select value={date} onChange={handleDateChange}>
+              <DatesOptions dates={dates} />
+            </select>
+          </label>
+          <div className="journey-picker__controls">
+            <button
+              className="btn"
+              type="submit"
+              disabled={!fromCity || !toCity || !date}
+            >
+              Vyhledat spoj
+            </button>
+          </div>
+        </form>
+        <img className="journey-picker__map" src={mapImage} />
+      </div>
     </div>
-  </div>
-)};
+  );
+};
